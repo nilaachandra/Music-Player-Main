02c8f979cc124f4a85ebad531ed7f63fe7ab1e88
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-01-10 23:02:14 +0530

    more functionalities addedd

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ console.log("afdfsdfdsf");
 let currentAudio = null; // Variable to store the currently playing audio
 let currentSongIndex = 0; // Variable to store the index of the currently playing song
 let isPlaying = false; // Variable to store the current playing state
+let isShuffle = false; // Variable to store the shuffle state
+let isRepeat = false; // Variable to store the repeat state
 
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
@@ -27,7 +29,27 @@ async function main() {
   let songsContainer = document.getElementById("songlists");
   let skipButton = document.getElementById("skip");
   let prevButton = document.getElementById("prev");
-  let pauseButton = document.getElementById("pause"); // Add pause button element in HTML
+  let pauseButton = document.getElementById("pause");
+  let shuffleButton = document.getElementById("shuffle"); // Add shuffle button element in HTML
+  let repeatButton = document.getElementById("repeat"); // Add repeat button element in HTML
+
+  // Function to toggle the color of the shuffle button
+  function toggleShuffleColor() {
+    shuffleButton.classList.toggle("text-green-500", isShuffle);
+    shuffleButton.classList.toggle("text-white", !isShuffle);
+  }
+
+  // Function to toggle the color of the repeat button
+  function toggleRepeatColor() {
+    repeatButton.classList.toggle("text-green-500", isRepeat);
+    repeatButton.classList.toggle("text-white", !isRepeat);
+  }
+
+  // Function to toggle the color of the pause button
+  function togglePauseColor() {
+    pauseButton.classList.toggle("text-green-500", isPlaying);
+    pauseButton.classList.toggle("text-white", !isPlaying);
+  }
 
   // Iterate through the songs array and create elements for each song
   songs.forEach((songURL, index) => {
@@ -64,7 +86,7 @@ async function main() {
   skipButton.addEventListener("click", () => {
     if (currentAudio) {
       currentAudio.pause();
-      currentSongIndex = (currentSongIndex + 1) % songs.length;
+      currentSongIndex = getNextSongIndex();
       playSong(currentSongIndex);
     }
   });
@@ -73,7 +95,7 @@ async function main() {
   prevButton.addEventListener("click", () => {
     if (currentAudio) {
       currentAudio.pause();
-      currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+      currentSongIndex = getPrevSongIndex();
       playSong(currentSongIndex);
     }
   });
@@ -88,7 +110,23 @@ async function main() {
         currentAudio.play();
         isPlaying = true;
       }
+      togglePauseColor(); // Toggle pause button color
     }
+  });
+
+  // Shuffle button functionality
+  shuffleButton.addEventListener("click", () => {
+    isShuffle = !isShuffle;
+    toggleShuffleColor(); // Toggle shuffle button color
+    if (isShuffle) {
+      shuffleSongs();
+    }
+  });
+
+  // Repeat button functionality
+  repeatButton.addEventListener("click", () => {
+    isRepeat = !isRepeat;
+    toggleRepeatColor(); // Toggle repeat button color
   });
 
   function playSong(index) {
@@ -103,7 +141,50 @@ async function main() {
     currentAudio = audio;
     currentSongIndex = index;
     isPlaying = true;
-  }
+
+    // If repeat is on, set the 'ended' event listener to replay the same song
+    if (isRepeat) {
+      currentAudio.addEventListener("ended", () => {
+        playSong(currentSongIndex);
+      });
+    }
+
+// If shuffle is on, shuffle the songs
+if (isShuffle) {
+  shuffleSongs();
+}
+
+// Toggle the colors of the buttons
+toggleShuffleColor();
+toggleRepeatColor();
+togglePauseColor();
+}
+
+function getNextSongIndex() {
+if (isShuffle) {
+  return getRandomIndex();
+} else {
+  return (currentSongIndex + 1) % songs.length;
+}
+}
+
+function getPrevSongIndex() {
+if (isShuffle) {
+  return getRandomIndex();
+} else {
+  return (currentSongIndex - 1 + songs.length) % songs.length;
+}
+}
+
+function getRandomIndex() {
+return Math.floor(Math.random() * songs.length);
+}
+
+function shuffleSongs() {
+if (isShuffle) {
+  currentSongIndex = getRandomIndex();
+}
+}
 }
 
 // Call the main function
